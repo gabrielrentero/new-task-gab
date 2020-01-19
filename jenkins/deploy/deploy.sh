2be217beb1034766bc677e5bf83d7f0458dfd280
Gabriel <gabrielmrentero@gmail.com>
2020-01-19 19:00:19 +0000

    Fix key path and deployment server info

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,6 +4,6 @@ echo thoughtworks-task > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@linuxfacilito.online:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@linuxfacilito.online:/tmp/publish
-ssh -i /opt/prod prod-user@linuxfacilito.online "/tmp/publish"
+scp -i /opt/task-key /tmp/.auth vagrant@192.168.2.225:/tmp/.auth
+scp -i /opt/task-key ./jenkins/deploy/publish vagrant@192.168.2.225:/tmp/publish
+ssh -i /opt/task-key vagrant@192.168.2.225 "/tmp/publish"
