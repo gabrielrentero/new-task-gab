cae97618e409bd594743c6e2439aca4cbdd12895
Gabriel <gabrielmrentero@gmail.com>
2020-01-19 05:52:05 +0000

    Fix path for java-app

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/var/jenkins_home/workspace/thoughtworks-task
+WORKSPACE=/tmp/thoughtworks-task
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Testing the code ***********"
 echo "***************************"
 
-WORKSPACE=/var/jenkins_home/workspace/thoughtworks-task
+WORKSPACE=/tmp/thoughtworks-task
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
