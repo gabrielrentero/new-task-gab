2b397873e768d0768bc49b88acf31f5e131b8de9
Gabriel <gabrielmrentero@gmail.com>
2020-01-20 00:24:37 +0000

    Skipping hot key

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,7 +5,7 @@ echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 echo $@ >> /tmp/.auth
 
-scp -i /opt/task-key /tmp/.auth vagrant@192.168.2.225:/tmp/.auth
+scp -i /opt/task-key -o "StrictHostKeyChecking no" /tmp/.auth vagrant@192.168.2.225:/tmp/.auth
 scp -i /opt/task-key ./jenkins/deploy/publish vagrant@192.168.2.225:/tmp/publish
 scp -i /opt/task-key ./jenkins/deploy/docker-compose.yml vagrant@192.168.2.225:/tmp/docker-compose.yml
 ssh -i /opt/task-key vagrant@192.168.2.225 "/tmp/publish"
